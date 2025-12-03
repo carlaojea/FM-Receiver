3877abfd8265c3ef62687539647635e2278c4627
carlaojea <carlaojea19@gmail.com>
2025-12-03 22:17:02 +0100

    Create sh1106_english.py

---
+++ sh1106_english.py
@@ -0,0 +1,63 @@
+# Driver for SH1106 (1.3" OLED screens)
+from machine import I2C
+import framebuf
+
+class SH1106(framebuf.FrameBuffer):
+    def __init__(self, width, height, i2c, addr=0x3c, rotate=0):
+        self.width = width
+        self.height = height
+        self.i2c = i2c
+        self.addr = addr
+        self.pages = self.height // 8
+        self.buffer = bytearray(self.pages * self.width)
+        super().__init__(self.buffer, self.width, self.height, framebuf.MONO_VLSB)
+        self.init_display()
+
+    def init_display(self):
+        self.write_cmd(0xAE) # Display OFF
+        self.write_cmd(0xA8) # Multiplex Ratio
+        self.write_cmd(0x3F) # 64 duty
+        self.write_cmd(0xD3) # Display Offset
+        self.write_cmd(0x00)
+        self.write_cmd(0x40) # Start Line
+        self.write_cmd(0xA1) # Segment remap
+        self.write_cmd(0xC8) # COM Output scan direction
+        self.write_cmd(0xDA) # COM Pins hardware config
+        self.write_cmd(0x12)
+        self.write_cmd(0x81) # Contrast
+        self.write_cmd(0xFF)
+        self.write_cmd(0xA4) # Entire Display ON
+        self.write_cmd(0xA6) # Normal/Inverse
+        self.write_cmd(0xD5) # Osc Frequency
+        self.write_cmd(0x80)
+        self.write_cmd(0x8D) # Charge Pump enable
+        self.write_cmd(0x14)
+        self.write_cmd(0xAF) # Display ON
+
+    def show(self):
+        for page in range(self.pages):
+            self.write_cmd(0xB0 + page)
+            self.write_cmd(0x02) # Low col addr (2 px offset for SH1106)
+            self.write_cmd(0x10) # High col addr
+            self.i2c.writeto(self.addr, b'\x40' + self.buffer[page * self.width:(page + 1) * self.width])
+
+    def write_cmd(self, cmd):
+        self.i2c.writeto(self.addr, b'\x00' + bytes([cmd]))
+
+    def poweroff(self):
+        self.write_cmd(0xAE)
+
+    def poweron(self):
+        self.write_cmd(0xAF)
+
+    def contrast(self, contrast):
+        self.write_cmd(0x81)
+        self.write_cmd(contrast)
+
+    def invert(self, invert):
+        self.write_cmd(0xA6 | (invert & 1))
+
+# Helper class compatible with standard constructor
+class SH1106_I2C(SH1106):
+    def __init__(self, width, height, i2c, addr=0x3c):
+        super().__init__(width, height, i2c, addr)
