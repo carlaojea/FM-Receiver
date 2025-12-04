30d76ea95fe1aa3819ed33051f74ff62885953d2
Guilloo <guilloestudiateleco@gmail.com>
2025-12-04 15:39:17 +0100

    Update controller_final.py

--- controller_final.py
+++ controller_final.py
@@ -1,4 +1,4 @@
-# Archivo: controller.py
+# File: controller.py
 from machine import Pin
 import time
 
@@ -7,7 +7,7 @@ class RadioController:
         self.radio = radio
         self.ui = ui
         
-        # Button PINS
+        # Buttons PIN
         self.btn_vol_up   = Pin(13, Pin.IN, Pin.PULL_UP)
         self.btn_vol_down = Pin(12, Pin.IN, Pin.PULL_UP)
         self.btn_freq_up  = Pin(27, Pin.IN, Pin.PULL_UP)
@@ -15,18 +15,18 @@ class RadioController:
         
         self.vol_actual = 8
         self.radio.set_vol(self.vol_actual)
-        self.radio.tune(98.5)
+        self.radio.tune(97.0)
         
-        # Time and State variables
+        # Time and state variables
         self.timer_cambio_pantalla = time.ticks_ms()
-        self.modo_rds = False # False = Pantalla Normal, True = Pantalla RDS
-        self.texto_rds_cache = "..." # Keep last text read
+        self.modo_rds = False # False= RDS screen, True= UI screen
+        self.texto_rds_cache = "..." 
 
     def loop(self):
         accion_realizada = False
         mensaje_estado = ""
         
-        # BUTTONS LOGIC
+        # Buttons logic
         if self.btn_vol_up.value() == 0:
             if self.vol_actual < 15:
                 self.vol_actual += 1
@@ -42,42 +42,42 @@ class RadioController:
                 time.sleep(0.15)
 
         elif self.btn_freq_up.value() == 0:
-            mensaje_estado = "Buscando >>"
+            mensaje_estado = "Searching >>"
             self.ui.actualizar(0, 0, 0, mensaje_estado)
             self.radio.seek(True)
             accion_realizada = True
             time.sleep(0.3)
 
         elif self.btn_freq_down.value() == 0:
-            mensaje_estado = "<< Buscando"
+            mensaje_estado = "<< Searching"
             self.ui.actualizar(0, 0, 0, mensaje_estado)
             self.radio.seek(False)
             accion_realizada = True
             time.sleep(0.3)
             
-        # SCREEN MODES
+        # Screen modes
         
-        # If user presses a button it changes to UI screen
+        # If input received change to UI screen
         if accion_realizada:
             self.modo_rds = False
             self.timer_cambio_pantalla = time.ticks_ms()
         
-        # Every 5 seconds the screen changes between UI and RDS 
+        # If no input, every 5 seconds change screen
         now = time.ticks_ms()
         if time.ticks_diff(now, self.timer_cambio_pantalla) > 5000:
-            self.modo_rds = not self.modo_rds # TicksTrue/False
+            self.modo_rds = not self.modo_rds 
             self.timer_cambio_pantalla = now # Reset timer
 
-        # RDS Register Screen
-        
+        # RDS data
+        # Read every RDS cycle data
         rds_leido = self.radio.check_rds()
-        if rds_leido.strip() != "": # If text it updates
+        if rds_leido.strip() != "": # if data, update
             self.texto_rds_cache = rds_leido
 
-        # SCREEN UPDATE
+        # Screen Update
         
         if mensaje_estado != "":
-            # Seeking message
+            
             pass 
         elif self.modo_rds:
             # Show RDS screen
@@ -87,4 +87,4 @@ class RadioController:
             freq, _, rssi = self.radio.get_info()
             self.ui.actualizar(freq, self.vol_actual, rssi)
             
-        time.sleep(0.1) #Better for the code to breathe 
+        time.sleep(0.1)
